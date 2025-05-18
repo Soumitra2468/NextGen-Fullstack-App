4593dc700fabe79c01dcdeee29e3177967fd019f
Soumitra2468 <sssoumitra412111221@gmail.com>
2025-05-18 21:37:59 +0530

    Enhance user registration with error handling and image upload support

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,10 +1,51 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import User from "../models/user.model.js";
+import uploadFile from "../utils/cloudinary.js";
 
 const register = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "User registered successfully",
+  const { fullName, email, password } = req.body;
+  if (
+    [fullName, email, password].some((field) => {
+      field.trim() === "" || field === undefined || field === null;
+    })
+  ) {
+    throw new ApiError("All fields are required", 400);
+  }
+  const user = User.findOne({ email });
+  if (user) {
+    throw new ApiError("User already exists", 400);
+  }
+  const avtarLocalPath = req.file?.profileImage[0].path;
+  if (!avtarLocalPath) {
+    throw new ApiError("Profile image is required", 400);
+  }
+
+  const avtar = await uploadFile(avtarLocalPath);
+  if (!avtar) {
+    throw new ApiError("Failed to upload image", 500);
+  }
+  const newUser = await User.create({
+    fullName,
+    email,
+    password,
+    profileImage: avtar.url,
   });
+
+  const cretedUser = User.findById(newUser._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!cretedUser) {
+    throw new ApiError("Failed to create user", 500);
+  }
+
+  return res.status(201).json(
+    new ApiResponse("User created successfully", 201, {
+      user: cretedUser,
+    })
+  );
 });
 
 export { register };
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,7 +1,15 @@
 import Router from "express";
 import { register } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/register").post(
+  upload.single("profileImage"),
+  (req, res, next) => {
+    req.body.profileImage = req.file.path;
+    next();
+  },
+  register
+);
 
 export default router;
